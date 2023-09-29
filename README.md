50e36394a50995d429cadd4b64100a19f9c846fc
James Mirarchi <jamesmirarchi@james-mbp.lan>
2023-09-29 18:41:04 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # grpc-go
 
+### Setup env
+```shell
+[ -n "$(go env GOBIN)" ] && export PATH="$(go env GOBIN):${PATH}"
+[ -n "$(go env GOPATH)" ] && export PATH="$(go env GOPATH)/bin:${PATH}"
+```
+
+### Build proto
+
+```shell
+─$ protoc event/v1/event.proto \
+  --go_out=. \
+  --go_opt=paths=source_relative \
+  --proto_path=. \
+--go-grpc_out=. --go-grpc_opt=paths=source_relative
+```
+
+## Server 
+```shell
+go run cmd/server/main.go 
+```
+
+## Client commands for testing
+
+```shell
+grpcurl -plaintext localhost:50001 event.v1.EventService/Retrieve
+
+grpcurl -plaintext localhost:50001 list
+grpcurl -plaintext localhost:50001 describe event.v1.EventService
+grpcurl -plaintext localhost:50001 list event.v1.EventService
+```
---
+++ cmd/server/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	pb "grpc-go/cmd/server/pb/event"
+	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	pb.UnimplementedEventServiceServer
+}
+
+func (s *server) Retrieve(ctx context.Context, in *pb.RetrieveRequest) (*pb.RetrieveResponse, error) {
+	log.Printf("Received Request: %v", in.ProtoReflect().Descriptor().FullName())
+	return &pb.RetrieveResponse{
+		Flights: getFlights(),
+	}, nil
+}
+
+func getFlights() []*pb.Flight {
+	sampleFlights := []*pb.Flight{
+		{
+			Id:         "1",
+			Pilot:      "Viper",
+			Ac:         "F18",
+			LastUpdate: time.Now().AddDate(0, -4, 0).String(),
+			Squadron:   "abc",
+			FuelStatus: float32(0.95),
+		},
+		{
+			Id:         "2",
+			Pilot:      "Goose",
+			Ac:         "F18",
+			LastUpdate: time.Now().AddDate(0, -1, 4).String(),
+			Squadron:   "abc",
+			FuelStatus: float32(0.93),
+		},
+		{
+			Id:         "3",
+			Pilot:      "Maverick",
+			Ac:         "F18",
+			LastUpdate: time.Now().AddDate(0, -4, 2).String(),
+			Squadron:   "abc",
+			FuelStatus: float32(0.90),
+		},
+	}
+	return sampleFlights
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":50001")
+	if err != nil {
+		log.Fatalln("error starting listener: ", err.Error())
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	pb.RegisterEventServiceServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
